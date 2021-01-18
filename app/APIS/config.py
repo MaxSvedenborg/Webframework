3097a60c9b07582d832a25941a8457ee3cb58007
Max Svedenborg <Max.Svedenborg@gmail.com>
2021-01-18 22:45:13 +0100

    Swapped repository, initialised project on new repository

---
+++ app/APIS/config.py
@@ -0,0 +1,6 @@
+# This is a placeholder file. It should be removed and credientials dynamically fetched from the DB.
+# WOOCOMMERSE API CREDENTIALS as strings
+URL = "https://thomthom.se/"
+CONSUMER_KEY = "ck_36303e4c6df96915fdc25e97502386d79daf69c3"
+CONSUMER_SECRET = "cs_ae6c69bd795c2e8ab9b281548b32956cbf02e073"
+VERSION = "wc/v3"
